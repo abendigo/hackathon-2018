1213c569c3188f6a1edaa0595760f18022331e39
Daniel Pang <daniel.pang@mac-1292-dp.local>
2018-06-27 12:27:54 -0400

    updated engine.py

--- backend/engine/engine.py
+++ backend/engine/engine.py
@@ -12,18 +12,33 @@ print(len(vals))
 # print(vals)
 
 
-def img_to_note(img):
+def avg_space_between_bars(pixels, bar_dict):
     '''
-    Accepts a PIL image object and returns a musical note (output format TBD)
+    Accepts a list of list representing the pixel data and a dict with keys being
+    the row column and values being the tuples indicating the pixel row range
+    Returns the average number of pixel rows between two bars
     '''
-    if img.size != (600,350):
-        return 'Incorrect image size'
+    space = 0
+    for key in bar_dict:
+        if key == 1:
+            pass
+        else:
+            space += (bar_dict[key][0] - bar_dict[key - 1][1])
 
-    # Convert to greyscale values and store it in a list
-    grey_img = img.convert('L')
-    grey_vals = list(grey_img.getdata())
+    return space/4
 
-    # Count number of dark pixels in each row (black is 0, white is 255)
-    for row in range(350):
-		pass
-        if count(grey_vals[row*600], )
+def img_to_note(pixels, bar_dict):
+    '''
+    Accepts a list of list representing the pixel data and a dict with keys being
+    the row column and values being the tuples indicating the pixel row range
+    Returns a musical note (output format TBD)
+    '''
+    # Input handling
+    if len(pixels) != 600 or len(pixels[0] != 900):
+        print 'Incorrect image size'
+        return
+
+    # Count number of dark pixels in each row (black is 1, white is 0)
+    for row in pixels:
+        pixel_counts = Counter(row[230:])
+        # Check number of black pixels
