371b47dba2cac1a9cb7ee7e48bcbc6cec0b49d10
Daniel Pang <daniel.pang@mac-1292-dp.local>
2018-06-27 14:55:27 -0400

    added to img_to_note

--- backend/engine/engine.py
+++ backend/engine/engine.py
@@ -6,26 +6,23 @@ from collections import Counter
 # im = Image.open('/Users/daniel.pang/dev/hackathon-2018/sample/sample1.png')
 # pic = im.load()
 
-vals = list(im.getdata())
-print(len(vals))
+# vals = list(im.getdata())
+# print(len(vals))
 # print(im.size)
 # print(vals)
 
-
-def avg_space_between_bars(pixels, bar_dict):
+def find_note(start, end, bar_dict):
     '''
-    Accepts a list of list representing the pixel data and a dict with keys being
-    the row column and values being the tuples indicating the pixel row range
-    Returns the average number of pixel rows between two bars
+    Hard coded key placements
     '''
-    space = 0
-    for key in bar_dict:
-        if key == 1:
-            pass
-        else:
-            space += (bar_dict[key][0] - bar_dict[key - 1][1])
+    if start < bar_dict[0][0] and end_row < bar_dict[0][1]:
+        return 'G'
+    elif start < bar_dict[0][0] and end_row > bar_dict[0][1]
+        return 'F'
+    elif start > bar_dict[0][0] and
 
-    return space/4
+    for i in range(4):
+        if
 
 def img_to_note(pixels, bar_dict):
     '''
@@ -38,7 +35,48 @@ def img_to_note(pixels, bar_dict):
         print 'Incorrect image size'
         return
 
+    min = 105
+    max = 140
+    start_row = 0
+    end_row = 0
+
     # Count number of dark pixels in each row (black is 1, white is 0)
-    for row in pixels:
+    # If number of pixels is within threshold of min and max then a note has been found
+    pixel_range = {'G':0, 'E': 0, 'C':0, 'A':0 'F':0, 'D':0}
+    for row_index in range(len(pixels)):
+        row = pixels[row_index]
         pixel_counts = Counter(row[230:])
-        # Check number of black pixels
+        # Count number of black pixels in the row
+        num_black_pixels = pixel_counts[1]
+
+        # Map pixel row to a note
+        if row_index < bar_dict[0][0]:
+            pixel_range['G'] += num_black_pixels
+        elif row_index < bar_dict[1][0]:
+            pixel_range['E'] += num_black_pixels
+        elif row_index < bar_dict[2][0]:
+            pixel_range['C'] += num_black_pixels
+        elif row_index < bar_dict[3][0]:
+            pixel_range['A'] += num_black_pixels
+        elif row_index < bar_dict[4][0]:
+            pixel_range['F'] += num_black_pixels
+        else:
+            pixel_range['D'] += num_black_pixels
+
+    # find note from pixel range
+    total = sum(pixel_range.values())
+    for key in pixel_range:
+        pixel_range[key] = pixel_range[key]/total
+
+    top_2 = dict(sorted(pixel_range.iteritems(), key=operator.itemgetter(1), reverse=True)[:2])
+    print(top_2)
+
+
+
+
+
+    if !found_note:
+        print 'Could not find note'
+        return
+
+    note = find_note(start_row, end_row, bar_dict)
